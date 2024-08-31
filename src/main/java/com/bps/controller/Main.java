ad12cff9b8ff6949829ce38255cf60c36ea07149
ASM Tamzid <atkthegreat9999@gmail.com>
2024-08-31 18:04:44 +0600

    just start

---
+++ src/main/java/com/bps/controller/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bps.controller;
+
+
+
+/**
+ *
+ * @author ASM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
